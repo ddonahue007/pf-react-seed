751d3e2ac5c0582484f7dccd68ae9c426ca0b84a
Doug Donahue <ddonahue@redhat.com>
2020-03-30 14:39:09 -0400

    Additional changes to routes, (fix typo)

--- src/app/api.tsx
+++ src/app/api.tsx
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
 //   baseURL: `http://localhost:8080/api`
-  baseURL: `http://customer-demo-ddonahue.apps.rhamilto.devcluster.openshift.com/api/customer/api`
+  baseURL: `http://customer-demo-ddonahue.apps.rhamilto.devcluster.openshift.com/api`
 });
