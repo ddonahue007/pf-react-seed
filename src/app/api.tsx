74d986b69a870cf5c42a8359d27514e4e42730f8
Doug Donahue <ddonahue@redhat.com>
2020-03-31 12:28:47 -0400

    Updating Rest URL...

--- src/app/api.tsx
+++ src/app/api.tsx
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
 //   baseURL: `http://localhost:8080/api`
-  baseURL: `http://customer-demo-ddonahue.apps.jvhadvig.devcluster.openshift.com/api`
+  baseURL: `http://customer-demo-ddonahue.apps.jhadvig-version-43.devcluster.openshift.com/api`
 });
