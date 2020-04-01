94e18efba0cb384bc634fde105930c4061f0fa41
Doug Donahue <ddonahue@redhat.com>
2020-04-01 12:50:48 -0400

    Updating Rest URL... testing something out

--- src/app/api.tsx
+++ src/app/api.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-//   baseURL: `http://localhost:8080/api`
-  baseURL: `https://console-openshift-console.apps.jai-0401.devcluster.openshift.com/api`
+  baseURL: `http://0.0.0.0:8080/api`
+//   baseURL: `https://console-openshift-console.apps.jai-0401.devcluster.openshift.com/api`
 });
